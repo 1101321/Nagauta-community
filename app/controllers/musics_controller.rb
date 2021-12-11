5f244e637f34235ecb9497c25bad2d1104a7a7b5
1101321 <hcri0909@gmail.com>
2021-12-11 14:14:32 +0000

    [Add] 楽曲投稿機能の作成

--- app/controllers/musics_controller.rb
+++ app/controllers/musics_controller.rb
@@ -1,24 +1,38 @@
 class MusicsController < ApplicationController
-  
-  def new
-  end
-  
+
   def create
+    @music = Music.new(music_params)
+    @music.user_id = current_user.id
+    @user = current_user
+    if @music.save
+      redirect_to music_path(@music.id)
+    else
+      @musics = Music.all
+      render :index
+    end
   end
-  
+
   def index
   end
-  
+
   def show
+    @music = Music.find(params[:id])
+    @user = current_user
   end
-  
+
   def edit
   end
-  
+
   def update
   end
-  
+
   def destroy
   end
-  
+
+  private
+
+  def music_params
+    params.require(:music).permit(:title, :body)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+
   #トップページ・アバウトページ(homes)
   root :to => 'homes#top'
   get '/about' => 'homes#about'
-  
+
   #楽曲(music)
   resources :musics, only: [:create, :index, :show, :edit, :update, :destroy]
-  
+  get '/musics' => 'musics#new', as: 'new_music'
+
   ##ユーザー(users)
   resources :users, only: [:show, :index, :edit, :update]
-  
+
 end
