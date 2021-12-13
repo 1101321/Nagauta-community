93944c3e0487520d94e1bbc15fcfac4d2898e1e5
1101321 <hcri0909@gmail.com>
2021-12-13 08:55:47 +0000

    [Add] User infoにプロフィール画像と自己紹介文を表示

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'ad05a91fa65acf5ff5650ea1bb42589d35069ff8d53d07cef5e5631a24b5b640f925dc9d55e94fa78730dfe017c1a380db7c6d3804ade5c1da5d6e7b37efe3d9'
