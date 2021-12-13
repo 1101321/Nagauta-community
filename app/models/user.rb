5ce7a598d2bc56ca84fb93e89616aaafe84b4af2
1101321 <hcri0909@gmail.com>
2021-12-13 09:43:02 +0000

    [Add] フォロー・フォロワー機能のアソシエーションとメソッドの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,28 @@ class User < ApplicationRecord
   
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
+  
+  #フォローした、されたの関連付け
+  #class_name: "Relationship"でRelationshipテーブルを参照
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  has_many :followings, through: :relationships, source: :followed
+  
+  #フォローしたとき
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+  
+  #フォローを外すとき
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+  
+  #フォローしているかの判定
+  def following?(user)
+    followings.include?(user)
+  end
+  
 end
