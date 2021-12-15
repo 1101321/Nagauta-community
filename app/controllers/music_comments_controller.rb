bf5850d9c99deb70fc758cbe0506cf8231dfa72a
1101321 <hcri0909@gmail.com>
2021-12-15 10:02:47 +0000

    [Add] コメント機能の非同期通信化

--- app/controllers/music_comments_controller.rb
+++ app/controllers/music_comments_controller.rb
@@ -1,19 +1,19 @@
 class MusicCommentsController < ApplicationController
   
   def create
-    music = Music.find(params[:music_id])
-    comment = MusicComment.new(music_comment_params)
-    comment.user_id = current_user.id
-    comment.music_id = music.id
-    comment.save
-    redirect_back(fallback_location: root_path)
+    @music = Music.find(params[:music_id])
+    @comment = MusicComment.new(music_comment_params)
+    @comment.user_id = current_user.id
+    @comment.music_id = @music.id
+    @comment.save
+    #redirect_back(fallback_location: root_path)
   end
   
   def destroy
-    music = Music.find(params[:music_id])
-    comment = music.music_comments.find(params[:id])
+    @music = Music.find(params[:music_id])
+    comment = @music.music_comments.find(params[:id])
     comment.destroy
-    redirect_back(fallback_location: root_path)
+    #redirect_back(fallback_location: root_path)
   end
   
   private
