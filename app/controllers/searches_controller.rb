f3ff8a02aab4c9129f72ed138081bba05fd8a4cd
1101321 <hcri0909@gmail.com>
2021-12-15 06:03:00 +0000

    [Add] searchesコントローラの作成

---
+++ app/controllers/searches_controller.rb
@@ -0,0 +1,40 @@
+class SearchesController < ApplicationController
+
+  def search
+    @model = params["search"]["model"]          #検索内容
+    @value = params["search"]["value"]          #条件
+    @how = params["search"]["how"]              #検索したいモデル
+    @dates = search_for(@how, @model, @value)
+  end
+
+  private
+
+  #完全一致
+  def match(model, value)
+    if model == "user"
+      User.where(name: value)
+    elsif model == "music"
+      Book.where(title: value)
+    end
+  end
+
+  #部分一致
+  def partical(model, value)
+    if model == 'user'
+      User.where("name LIKE ?", "%#{value}%")
+    elsif model == 'book'
+      Book.where("title LIKE ?", "%#{value}%")
+    end
+  end
+
+  def search_for(how, model, value)
+    case how
+    when 'match'
+      match(model, value)
+    when 'partical'
+      partical(model, value)
+    end
+  end
+
+
+end
