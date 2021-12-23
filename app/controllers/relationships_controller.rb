c80e10bbe1b1a43621fdb87dc89fa9a88e1da0cc
1101321 <hcri0909@gmail.com>
2021-12-23 08:37:27 +0000

    [Update] ユーザ関連のページネーション・フォロー、フォロワーページに検索フォームを追加

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -15,13 +15,13 @@ class RelationshipsController < ApplicationController
   #フォローユーザー一覧
   def followings
     @user = User.find(params[:user_id])
-    @users = @user.followings
+    @users = @user.followings.page(params[:page]).per(10)
   end
   
   #フォロワー一覧
   def followers
     @user = User.find(params[:user_id])
-    @users = @user.followers
+    @users = @user.followers.page(params[:page]).per(10)
   end
   
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users = User.all
+    @users = User.all.page(params[:page]).per(10)
     @user = current_user
   end
 
