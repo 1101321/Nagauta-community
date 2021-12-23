a65a29f718511f97fe6bafa9be8065e9b82f494b
1101321 <hcri0909@gmail.com>
2021-12-23 10:51:31 +0000

    [Update] トップページと概要ページの作成

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,10 +1,11 @@
 class HomesController < ApplicationController
-  
+
   def top
     @user = current_user
+    @musics = Music.order(created_at: :desc).limit(2)
   end
-  
+
   def about
   end
-  
+
 end
